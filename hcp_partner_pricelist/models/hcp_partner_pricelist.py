062966d82a7e70028016b495d86f538d8588f0d7
asyoung58 <asyoung@armorconcepts.com>
2020-09-08 20:43:39 +0530

    [UPDATE] Removed Custom Tax Feature

--- hcp_partner_pricelist/models/hcp_partner_pricelist.py
+++ hcp_partner_pricelist/models/hcp_partner_pricelist.py
@@ -377,13 +377,20 @@ class SaleOrder(models.Model):
             'view_id': form_view_id,
             'target': 'new',
         }
-     
+
+    
     def _create_delivery_line(self, carrier, price_unit):
         SaleOrderLine = self.env['sale.order.line']
         if self.partner_id:
             # set delivery detail in the customer language
             carrier = carrier.with_context(lang=self.partner_id.lang)
 
+        # Apply fiscal position
+        taxes = carrier.product_id.taxes_id.filtered(lambda t: t.company_id.id == self.company_id.id)
+        taxes_ids = taxes.ids
+        if self.partner_id and self.fiscal_position_id:
+            taxes_ids = self.fiscal_position_id.map_tax(taxes, carrier.product_id, self.partner_id).ids
+            
         # Create the sales order line
         carrier_with_partner_lang = carrier.with_context(lang=self.partner_id.lang)
         if carrier_with_partner_lang.product_id.description_sale:
@@ -391,7 +398,6 @@ class SaleOrder(models.Model):
                                         carrier_with_partner_lang.product_id.description_sale)
         else:
             so_description = carrier_with_partner_lang.name
-        taxes_ids = self.partner_id.taxes_id.ids
         values = {
             'order_id': self.id,
             'name': so_description,
@@ -399,7 +405,7 @@ class SaleOrder(models.Model):
             'product_uom': carrier.product_id.uom_id.id,
             'product_id': carrier.product_id.id,
             'tax_id': [(6, 0, taxes_ids)],
-	    'product_ship_method': True,
+            'product_ship_method': True,
             'is_delivery': True,
         }
         if carrier.invoice_policy == 'real':
