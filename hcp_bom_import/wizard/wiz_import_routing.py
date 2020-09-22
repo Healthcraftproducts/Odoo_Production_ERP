12235d38a258dbb4b67111e7ee524a8058f9bf83
asyoung58 <asyoung@armorconcepts.com>
2020-09-22 19:33:33 +0530

    [ADD] validation added for bom singleton

--- hcp_bom_import/wizard/wiz_import_routing.py
+++ hcp_bom_import/wizard/wiz_import_routing.py
@@ -177,6 +177,8 @@ class ImportBomData(models.TransientModel):
 						    sheet2_itemcode = line2[0]
 						    if itemcode == line2[0]:
 						        main_product = itemcode
+						        if not main_product:
+						            raise ValidationError(_('Product (%s).') % line2[0])
 						        prod = self.env['product.product'].search([('default_code','=',str(main_product))])
 						        bom = self.env['mrp.bom'].search([('product_id','=',prod.id)])
 						       # print(bom,'BOM###########################')
