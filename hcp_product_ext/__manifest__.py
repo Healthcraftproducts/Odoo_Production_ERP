0eb23b209194d0ec765820d2adc0fb123fd30868
VineelKumar-navabrindit <66935938+VineelKumar-navabrindit@users.noreply.github.com>
2020-10-20 19:49:51 +0530

    updated product.py,product_views.xml,manifest.py
    Changes done for expiration date,created fields for length,width,height,pallet shippment.total weight for shipping, changes internal ref to source ref, minimum quantity in inventory report.

--- hcp_product_ext/__manifest__.py
+++ hcp_product_ext/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','product','sale','stock','purchase'],
+    'depends': ['base','product','sale','stock','purchase','delivery'],
 
     # always loaded
     'data': [
