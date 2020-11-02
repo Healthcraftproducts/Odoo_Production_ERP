71d772259e06609dab927f694a38a91fa254a922
VineelKumar-navabrindit <66935938+VineelKumar-navabrindit@users.noreply.github.com>
2020-11-02 17:10:27 +0530

    setuptime got added in BOM structure & cost
    setuptime got added in BOM structure & cost

--- hcp_mrp_module/models/mrp_production_inherit.py
+++ hcp_mrp_module/models/mrp_production_inherit.py
@@ -154,3 +154,21 @@ class MrpBom(models.Model):
 			for line in self.bom_line_ids:
 				line.product_uom_id=line.product_id.uom_id
    
+class ReportBomStructure(models.AbstractModel):
+	_inherit = 'report.mrp.report_bom_structure'
+
+	def _get_operation_line(self, routing, qty, level):
+		operations = []
+		total = 0.0
+		for operation in routing.operation_ids:
+			operation_cycle = float_round(qty / operation.workcenter_id.capacity, precision_rounding=1, rounding_method='UP')
+			duration_expected = operation_cycle * operation.time_cycle + operation.workcenter_id.time_stop + operation.workcenter_id.time_start + operation.setup_time
+			total = ((duration_expected / 60.0) * operation.workcenter_id.costs_hour)
+			operations.append({
+				'level': level or 0,
+				'operation': operation,
+				'name': operation.name + ' - ' + operation.workcenter_id.name,
+				'duration_expected': duration_expected,
+				'total': self.env.company.currency_id.round(total),
+			})
+		return operations
