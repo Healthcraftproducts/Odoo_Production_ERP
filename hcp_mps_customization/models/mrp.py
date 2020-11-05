08e47860bb708aaae2c621fc0d385d97f0aae46b
asyoung58 <asyoung@armorconcepts.com>
2020-11-05 18:28:31 +0530

    [ADD] Sales Order Error Fix On MPS Customization 4

--- hcp_mps_customization/models/mrp.py
+++ hcp_mps_customization/models/mrp.py
@@ -168,7 +168,56 @@ class StockRule(models.Model):
             )
             merged_procurements.append(merged_procurement)
         return merged_procurements
-    
+
+    def _get_stock_move_values(self, product_id, product_qty, product_uom, location_id, name, origin, company_id, values,mo_reference):
+        ''' Returns a dictionary of values that will be used to create a stock move from a procurement.
+        This function assumes that the given procurement has a rule (action == 'pull' or 'pull_push') set on it.
+
+        :param procurement: browse record
+        :rtype: dictionary
+        '''
+        group_id = False
+        if self.group_propagation_option == 'propagate':
+            group_id = values.get('group_id', False) and values['group_id'].id
+        elif self.group_propagation_option == 'fixed':
+            group_id = self.group_id.id
+
+        date_expected = fields.Datetime.to_string(
+            fields.Datetime.from_string(values['date_planned']) - relativedelta(days=self.delay or 0)
+        )
+        # it is possible that we've already got some move done, so check for the done qty and create
+        # a new move with the correct qty
+        qty_left = product_qty
+        move_values = {
+            'name': name[:2000],
+            'company_id': self.company_id.id or self.location_src_id.company_id.id or self.location_id.company_id.id or company_id.id,
+            'product_id': product_id.id,
+            'product_uom': product_uom.id,
+            'product_uom_qty': qty_left,
+            'partner_id': self.partner_address_id.id or (values.get('group_id', False) and values['group_id'].partner_id.id) or False,
+            'location_id': self.location_src_id.id,
+            'location_dest_id': location_id.id,
+            'move_dest_ids': values.get('move_dest_ids', False) and [(4, x.id) for x in values['move_dest_ids']] or [],
+            'rule_id': self.id,
+            'procure_method': self.procure_method,
+            'origin': origin,
+            'picking_type_id': self.picking_type_id.id,
+            'group_id': group_id,
+            'route_ids': [(4, route.id) for route in values.get('route_ids', [])],
+            'warehouse_id': self.propagate_warehouse_id.id or self.warehouse_id.id,
+            'date': date_expected,
+            'date_expected': date_expected,
+            'propagate_cancel': self.propagate_cancel,
+            'propagate_date': self.propagate_date,
+            'propagate_date_minimum_delta': self.propagate_date_minimum_delta,
+            'description_picking': product_id._get_description(self.picking_type_id),
+            'priority': values.get('priority', "1"),
+            'delay_alert': self.delay_alert,
+        }
+        for field in self._get_custom_move_fields():
+            if field in values:
+                move_values[field] = values.get(field)
+        return move_values    
     
 class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
