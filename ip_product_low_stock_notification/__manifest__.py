ae7733c069f663d8b73218bc87c5d187440b5555
asyoung58 <asyoung@armorconcepts.com>
2020-11-05 21:59:46 +0530

    [ADD] min stock report and muti MO process app added

---
+++ ip_product_low_stock_notification/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Product Low Stock Report/ Notification',
+    'summary': "Product Low Stock Report/ Notification based on minimum quantity. Get stock by selected company and location and notify to selected user",
+    'description': """Product Low Stock Report/ Notification based on minimum quantity. Get stock by selected company and location and notify to selected user""",
+
+    'author': 'iPredict IT Solutions Pvt. Ltd.',
+    'website': 'http://ipredictitsolutions.com',
+    "support": "ipredictitsolutions@gmail.com",
+
+    'category': 'Warehouse',
+    'version': '13.0.0.1.2',
+    'depends': ['stock'],
+    'data': [
+        'wizard/product_low_stock_wiz_view.xml',
+        'views/product_view.xml',
+        'views/stock_config_settings_view.xml',
+        'report/report_action_view.xml',
+        'report/report_product_low_stock.xml',
+        'data/low_stock_notification_data.xml',
+    ],
+
+    'license': "OPL-1",
+    'price': 40,
+    'currency': "EUR",
+
+    'auto_install': False,
+    'installable': True,
+
+    'images': ['static/description/main.png'],
+}
---
+++ ip_product_low_stock_notification/models/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+from . import product_low_stock
+from . import stock_config_settings
