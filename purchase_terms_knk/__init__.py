88428a7037e4a57691f12dcd99bba7e93d021720
VineelKumar-navabrindit <66935938+VineelKumar-navabrindit@users.noreply.github.com>
2020-11-18 18:05:09 +0530

    Added PO terms APP
    Added PO terms APP

---
+++ purchase_terms_knk/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+# Author      : Kanak Infosystems LLP. (<https://www.kanakinfosystems.com/>)
+# Copyright(c): 2012-Present Kanak Infosystems LLP.
+# All Rights Reserved.
+#
+#
+# This program is copyright property of the author mentioned above.
+# You can`t redistribute it and/or modify it.
+#
+#
+# You should have received a copy of the License along with this program.
+# If not, see <https://www.kanakinfosystems.com/license>
+###############################################################################
+
+from . import models
