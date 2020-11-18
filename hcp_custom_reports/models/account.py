166e4427c90dc6d113ba4e63114dd765c4581223
VineelKumar-navabrindit <66935938+VineelKumar-navabrindit@users.noreply.github.com>
2020-11-18 12:20:32 +0530

    Updated invoice report changes
    Updated invoice report changes

--- hcp_custom_reports/models/account.py
+++ hcp_custom_reports/models/account.py
@@ -44,40 +44,40 @@ class AccountMove(models.Model):
 
 
 
-	def action_invoice_sent(self):
-		""" Open a window to compose an email, with the edi invoice template
-			message loaded by default
-		"""
-		self.ensure_one()
-		template = self.env.ref('hcp_custom_reports.invoice_send_email_template_id', raise_if_not_found=False)
-		lang = get_lang(self.env)
-		if template and template.lang:
-			lang = template._render_template(template.lang, 'account.move', self.id)
-		else:
-			lang = lang.code
-		compose_form = self.env.ref('account.account_invoice_send_wizard_form', raise_if_not_found=False)
-		ctx = dict(
-			default_model='account.move',
-			default_res_id=self.id,
-			default_use_template=bool(template),
-			default_template_id=template and template.id or False,
-			default_composition_mode='comment',
-			mark_invoice_as_sent=True,
-			custom_layout="mail.mail_notification_paynow",
-			model_description=self.with_context(lang=lang).type_name,
-			force_email=True
-		)
-		return {
-			'name': _('Send Invoice'),
-			'type': 'ir.actions.act_window',
-			'view_type': 'form',
-			'view_mode': 'form',
-			'res_model': 'account.invoice.send',
-			'views': [(compose_form.id, 'form')],
-			'view_id': compose_form.id,
-			'target': 'new',
-			'context': ctx,
-		}
+	# def action_invoice_sent(self):
+	# 	""" Open a window to compose an email, with the edi invoice template
+	# 		message loaded by default
+	# 	"""
+	# 	self.ensure_one()
+	# 	template = self.env.ref('hcp_custom_reports.invoice_send_email_template_id', raise_if_not_found=False)
+	# 	lang = get_lang(self.env)
+	# 	if template and template.lang:
+	# 		lang = template._render_template(template.lang, 'account.move', self.id)
+	# 	else:
+	# 		lang = lang.code
+	# 	compose_form = self.env.ref('account.account_invoice_send_wizard_form', raise_if_not_found=False)
+	# 	ctx = dict(
+	# 		default_model='account.move',
+	# 		default_res_id=self.id,
+	# 		default_use_template=bool(template),
+	# 		default_template_id=template and template.id or False,
+	# 		default_composition_mode='comment',
+	# 		mark_invoice_as_sent=True,
+	# 		custom_layout="mail.mail_notification_paynow",
+	# 		model_description=self.with_context(lang=lang).type_name,
+	# 		force_email=True
+	# 	)
+	# 	return {
+	# 		'name': _('Send Invoice'),
+	# 		'type': 'ir.actions.act_window',
+	# 		'view_type': 'form',
+	# 		'view_mode': 'form',
+	# 		'res_model': 'account.invoice.send',
+	# 		'views': [(compose_form.id, 'form')],
+	# 		'view_id': compose_form.id,
+	# 		'target': 'new',
+	# 		'context': ctx,
+	# 	}
 
 
 
@@ -93,6 +93,14 @@ class AccountMoveLine(models.Model):
 
 	price_tax = fields.Float(string="Total Tax")
 	invoice_ship_method = fields.Boolean(string='Invoice ship',default=False,compute='ship_line_method')
+	inv_line_amount = fields.Float(string="Line Amount",compute='_compute_invoice_line_level_amount',store=True)
+
+
+	@api.depends('quantity','price_unit')
+	def _compute_invoice_line_level_amount(self):
+		for line in self:
+			line.inv_line_amount = line.quantity * line.price_unit
+
 
 
 	def ship_line_method(self):
