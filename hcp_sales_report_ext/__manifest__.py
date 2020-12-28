8608964f61ce89e32f9c186e6bf5e38b119408fe
hemkumarnits <hemkumar.m@navabrindit.com>
2020-12-28 22:12:19 +0530

    Sales Report Customization Added

---
+++ hcp_sales_report_ext/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "HCP Sales Report Extended",
+
+    'summary': """This module is used for sales report customization""",
+
+    'description': """This module is used for sales report customization""",
+
+    'author': "NITS PVT LTD",
+    'website': "http://www.navabrindsol.com",
+    'category': 'Sales',
+    'version': '0.1',
+    'depends': ['base','sale'],
+    'data': [
+        'views/sales_report.xml',   
+    ],   
+}
