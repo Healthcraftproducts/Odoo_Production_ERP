e9c6227c64fb4f7217a31c6f2d1c9f30d590001c
hemkumarnits <hemkumar.m@navabrindit.com>
2021-02-03 15:44:23 +0530

    Deringer Changes Added

--- hcp_product_ext/models/product.py
+++ hcp_product_ext/models/product.py
@@ -2,6 +2,11 @@
 
 from odoo import api, fields, models, SUPERUSER_ID, _
 
+class TarrifNumber(models.Model):
+	_name = 'tariff.number'
+	_description = "Tariff Number"
+
+	name = fields.Char(string="Tariff Number")
 
 class ProductTemplate(models.Model):
 	_inherit = 'product.template'
@@ -38,6 +43,7 @@ class ProductTemplate(models.Model):
 	product_sub_categ_id = fields.Many2one('product.sub.category',string="Product Sub Category")
 	obsolute_product = fields.Boolean('Obsolute Product')
 	categ_id = fields.Many2one('product.category', 'Account Category',change_default=True, default=_get_default_category_id, group_expand='_read_group_categ_id',required=True, help="Select category for the current product")
+	tarrif_number = fields.Many2many('tariff.number', 'tariff_number_rel2', 'product_id', 'tariff_id',string='Tariff Number', copy=False,)
 
 	@api.depends('product_variant_ids', 'product_variant_ids.default_code')
 	def _compute_default_code(self):
@@ -102,7 +108,8 @@ class ProductMaster(models.Model):
 	deringer_uom_id = fields.Many2one('deringer.uom','Deringer UOM')
 	usmca_eligible = fields.Selection([('yes','Yes'),('no','No')],'USMCA Eligible?')
 	manufacturer_id = fields.Char('MID')
-
+	tarrif_number = fields.Many2many('tariff.number', 'tariff_number_rel', 'product_id', 'tariff_id',string='Tariff Number', copy=False,)
+	
 	def active_stage(self):
 		self.write({
 		'status': '0',
