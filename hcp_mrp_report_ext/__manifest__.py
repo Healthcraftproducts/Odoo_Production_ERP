827a15aac058826298a76600a1fa418be87872ed
hemkumarnits <hemkumar.m@navabrindit.com>
2021-02-08 18:23:50 +0530

    MRP report customization updated to prod

---
+++ hcp_mrp_report_ext/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "HCP MRP Report Extended",
+
+    'summary': """This module is used for mrp report customization""",
+
+    'description': """This module is used for mrp report customization""",
+
+    'author': "NITS PVT LTD",
+    'website': "http://www.navabrindsol.com",
+    'category': 'Sales',
+    'version': '0.1',
+    'depends': ['base','mrp'],
+    'data': [
+        'report/workorder_report.xml',  
+        'report/report.xml',  
+    ],   
+}
