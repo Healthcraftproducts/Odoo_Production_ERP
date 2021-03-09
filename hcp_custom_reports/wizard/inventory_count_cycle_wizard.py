521f3e2bf3f023f321e3d8bdd768bf948a8eb335
VineelKumar-navabrindit <66935938+VineelKumar-navabrindit@users.noreply.github.com>
2021-03-09 17:01:22 +0530

    added changes of inventory cycle report
    added changes of inventory cycle report

--- hcp_custom_reports/wizard/inventory_count_cycle_wizard.py
+++ hcp_custom_reports/wizard/inventory_count_cycle_wizard.py
@@ -16,7 +16,7 @@ class InventoryReportOut(models.Model):
 	_description = 'Inventory Cycle Report Out'
 
 	inv_rep_data = fields.Char('Name', size=256)
-	file_name = fields.Binary('Inventory Count Cycle Report', readonly=True)
+	file_name = fields.Binary('Inventory Cycle Count Report', readonly=True)
 
 
 class InventoryCountCycleReportWizard(models.TransientModel):
@@ -44,18 +44,18 @@ class InventoryCountCycleReportWizard(models.TransientModel):
 					if st_date>=date_from and st_date<=date_to:
 						for line in stock.line_ids:
 							product = self.env['product.product'].search([('id','=',line.product_id.id)])
-							onhand_qty = sum(line.theoretical_qty for line in stock.line_ids if line.product_id.id in product.ids)
-							adj_qty = sum(line.product_qty for line in stock.line_ids if line.product_id.id in product.ids)
+							# onhand_qty = sum(line.theoretical_qty for line in stock.line_ids if line.product_id.id in product.ids)
+							# adj_qty = sum(line.product_qty for line in stock.line_ids if line.product_id.id in product.ids)
 							a=line.product_id.id
-							b=[stock.name,loc_name,line.product_id.default_code,line.product_id.name,onhand_qty,adj_qty,line.product_uom_id.name]
+							b=[stock.name,loc_name,line.inventory_date,line.prod_lot_id.name,line.product_id.default_code,line.product_id.name,line.theoretical_qty,line.product_qty,line.difference_qty,line.product_uom_id.name,line.product_id.standard_price,line.difference_qty*line.product_id.standard_price]
 							invent_details={'id':a,'values':b}
 							inv_list.append(invent_details)
 		list_inventory = inv_list
-		new_v =[]
-		for y in list_inventory:
-			if y not in new_v:
-				new_v.append(y)
-		for data in new_v:
+		# new_v =[]
+		# for y in list_inventory:
+		# 	if y not in new_v:
+		# 		new_v.append(y)
+		for data in list_inventory:
 			stock_list=[]
 			stock_list.append(data['values'][0])
 			stock_list.append(data['values'][1])
@@ -64,6 +64,11 @@ class InventoryCountCycleReportWizard(models.TransientModel):
 			stock_list.append(data['values'][4])
 			stock_list.append(data['values'][5])
 			stock_list.append(data['values'][6])
+			stock_list.append(data['values'][7])
+			stock_list.append(data['values'][8])
+			stock_list.append(data['values'][9])
+			stock_list.append(data['values'][10])
+			stock_list.append(data['values'][11])
 			stock_list1.append(stock_list)
 		
 		return_list['stock_ids'] = stock_list1
@@ -74,10 +79,12 @@ class InventoryCountCycleReportWizard(models.TransientModel):
 	
 	def print_inv_count_xls_report(self):
 		workbook = xlwt.Workbook()
-		sheet = workbook.add_sheet("Inventory Count Cycle Report")
+		sheet = workbook.add_sheet("Inventory Cycle Count Report")
 		format2 = xlwt.easyxf('font: bold 1')
 		format3 = xlwt.easyxf('font: bold 1; align: horiz right',)
 		style1 = xlwt.easyxf(num_format_str='#,##0.00')
+		style2 = xlwt.easyxf('align: horiz left',num_format_str='mm-dd-yyyy hh:mm:ss')
+		style3 = xlwt.easyxf(num_format_str ='"$"#,##0.00')
 		first_col = sheet.col(0)
 		two_col = sheet.col(1)
 		three_col = sheet.col(2)
@@ -85,39 +92,59 @@ class InventoryCountCycleReportWizard(models.TransientModel):
 		fifth_col = sheet.col(4)
 		sixth_col = sheet.col(5)
 		seventh_col = sheet.col(6)
+		eigth_col = sheet.col(7)
+		ninth_col = sheet.col(8)
+		tenth_col = sheet.col(9)
+		eleventh_col = sheet.col(10)
+		twelth_col = sheet.col(11)
 		first_col.width = 250*20
 		two_col.width = 250*20
-		three_col.width = 150*20
-		fourth_col.width = 400*20
+		three_col.width = 250*20
+		fourth_col.width = 200*20
 		fifth_col.width = 200*20
-		sixth_col.width = 200*20
+		sixth_col.width = 400*20
 		seventh_col.width = 200*20
+		eigth_col.width = 200*20
+		ninth_col.width = 200*20
+		tenth_col.width = 200*20
+		eleventh_col.width = 180*20
+		twelth_col.width = 250*20
 		sheet.write(0,0,'Inventory',format2)
 		sheet.write(0,1,'Location Name',format2)
-		sheet.write(0,2,'Item Code',format2)
-		sheet.write(0,3,'Product Name',format2)
-		sheet.write(0,4,'OnHand Qty',format3)
-		sheet.write(0,5,'Adjusted Qty',format3)
-		sheet.write(0,6,'Unit Of Measure',format2)
+		sheet.write(0,2,'Inventory Date',format2)
+		sheet.write(0,3,'Lot/Serial No',format2)
+		sheet.write(0,4,'Item Code',format2)
+		sheet.write(0,5,'Product Name',format2)
+		sheet.write(0,6,'OnHand Qty',format3)
+		sheet.write(0,7,'Counted Qty',format3)
+		sheet.write(0,8,'Difference Qty',format3)
+		sheet.write(0,9,'Unit Of Measure',format2)
+		sheet.write(0,10,'Product Cost',format3)
+		sheet.write(0,11,'$Difference(Price)',format3)
 		data = self.get_data()
 		dt = data.get('stock_ids')
 		row_number = 1
 		for value in dt:
 			sheet.write(row_number,0,value[0])
 			sheet.write(row_number,1,value[1])
-			sheet.write(row_number,2,value[2])
+			sheet.write(row_number,2,value[2],style2)
 			sheet.write(row_number,3,value[3])
-			sheet.write(row_number,4,value[4],style1)
-			sheet.write(row_number,5,value[5],style1)
-			sheet.write(row_number,6,value[6])
+			sheet.write(row_number,4,value[4])
+			sheet.write(row_number,5,value[5])
+			sheet.write(row_number,6,value[6],style1)
+			sheet.write(row_number,7,value[7],style1)
+			sheet.write(row_number,8,value[8],style1)
+			sheet.write(row_number,9,value[9])
+			sheet.write(row_number,10,value[10],style3)
+			sheet.write(row_number,11,value[11],style3)
 			row_number +=1
 
 
 		output = StringIO()
 		if platform.system() == 'Linux':
-			filename = ('/tmp/Inventory Count Cycle Report' +'.xls')
+			filename = ('/tmp/Inventory Cycle Count Report' +'.xls')
 		else:
-			filename = ('Inventory Count Cycle Report' +'.xls')
+			filename = ('Inventory Cycle Count Report' +'.xls')
 
 		workbook.save(filename)
 		fp = open(filename, "rb")
@@ -125,7 +152,7 @@ class InventoryCountCycleReportWizard(models.TransientModel):
 		out = base64.encodestring(file_data)
 		# Files actions
 		attach_vals = {
-				'inv_rep_data': 'Inventory Count Cycle Report'+ '.xls',
+				'inv_rep_data': 'Inventory Cycle Count Report'+ '.xls',
 				'file_name': out,
 				# 'purchase_work':'Purchase'+ '.csv',
 				# 'file_names':data,
