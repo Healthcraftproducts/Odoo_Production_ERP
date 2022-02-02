246651f7396e45696135c33d78aed95b14d1b1ae
Poovarasan <poovarasan.e@navabrindit.com>
2022-02-02 16:06:37 +0530

    operator group issue fixed

---
+++ hcp_operator_lot_restrict/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+# -*- encoding: utf-8 -*-
+{
+    'name': 'HCP Operator Lot Restriction',
+    'category': 'Manufacturing',
+    'version': '1.0',
+    'author' : 'nits',
+    'description': "HCP Operator Lot restriction for create",
+    'depends': ['stock','hcp_operator_customization','multiple_mrp_orders','mrp'],
+    'installable': True,
+    'data': [  
+        'security/ir.model.access.csv',
+        'views/view.xml',
+    ],
+    "installable": True,
+}
+
