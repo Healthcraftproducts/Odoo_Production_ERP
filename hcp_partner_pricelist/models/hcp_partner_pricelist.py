d2fb336ada0a3285c3059e889b8bb057c737528e
hemkumarnits <hemkumar.m@navabrindit.com>
2022-03-17 16:40:38 +0530

    Bug fix for pricelist in base module

--- hcp_partner_pricelist/models/hcp_partner_pricelist.py
+++ hcp_partner_pricelist/models/hcp_partner_pricelist.py
@@ -3,6 +3,65 @@ from odoo import api, fields, models, tools, _
 from odoo.tools.misc import formatLang, get_lang
 from odoo.exceptions import AccessError, UserError, ValidationError
 from datetime import datetime, timedelta, date
+from odoo.addons.sale.models.sale import SaleOrderLine as salesline
+from odoo.addons.sale.models.sale import SaleOrderLine as salesuom
+
+@api.onchange('product_id')
+def product_id_change(self):
+    if not self.product_id:
+        return
+    valid_values = self.product_id.product_tmpl_id.valid_product_template_attribute_line_ids.product_template_value_ids
+    # remove the is_custom values that don't belong to this template
+    for pacv in self.product_custom_attribute_value_ids:
+        if pacv.custom_product_template_attribute_value_id not in valid_values:
+            self.product_custom_attribute_value_ids -= pacv
+
+    # remove the no_variant attributes that don't belong to this template
+    for ptav in self.product_no_variant_attribute_value_ids:
+        if ptav._origin not in valid_values:
+            self.product_no_variant_attribute_value_ids -= ptav
+
+    vals = {}
+    if not self.product_uom or (self.product_id.uom_id.id != self.product_uom.id):
+        vals['product_uom'] = self.product_id.uom_id
+        vals['product_uom_qty'] = self.product_uom_qty or 1.0
+
+    product = self.product_id.with_context(
+        lang=get_lang(self.env, self.order_id.partner_id.lang).code,
+        partner=self.order_id.partner_id,
+        quantity=vals.get('product_uom_qty') or self.product_uom_qty,
+        date=self.order_id.date_order,
+        pricelist=self.order_id.pricelist_id.id,
+        uom=self.product_uom.id
+    )
+    #vals.update(name=self.get_sale_order_line_multiline_description_sale(product))
+    #HEM NEW
+    vals.update(name=self.product_id.name)
+    #self.tax_id = self.order_id.partner_id.taxes_id # Added to get tax from Customer 
+    self._compute_tax_id() # commented to not to derive tax from product
+
+    if self.order_id.pricelist_id and self.order_id.partner_id:
+        vals['price_unit'] = self.env['account.tax']._fix_tax_included_price_company(self._get_display_price(product), product.taxes_id, self.tax_id, self.company_id)
+    #raise ValidationError(_('#######%s')%vals['price_unit'])
+
+    self.update(vals)
+    #raise ValidationError(_('#######%s')% vals)
+    title = False
+    message = False
+    result = {}
+    warning = {}
+    if product.sale_line_warn != 'no-message':
+        title = _("Warning for %s") % product.name
+        message = product.sale_line_warn_msg
+        warning['title'] = title
+        warning['message'] = message
+        result = {'warning': warning}
+        if product.sale_line_warn == 'block':
+            self.product_id = False
+
+    return result
+
+salesline.product_id_change = product_id_change
 
 class Lead(models.Model):
     _inherit = "crm.lead"
@@ -426,65 +485,7 @@ class SaleOrder(models.Model):
             values['sequence'] = self.order_line[-1].sequence + 1
         sol = SaleOrderLine.sudo().create(values)
         return sol   
-    
-    
-class SaleOrderLine(models.Model):
-    _inherit = 'sale.order.line'
         
-    @api.onchange('product_id')
-    def product_id_change(self):
-        if not self.product_id:
-            return
-        valid_values = self.product_id.product_tmpl_id.valid_product_template_attribute_line_ids.product_template_value_ids
-        # remove the is_custom values that don't belong to this template
-        for pacv in self.product_custom_attribute_value_ids:
-            if pacv.custom_product_template_attribute_value_id not in valid_values:
-                self.product_custom_attribute_value_ids -= pacv
-
-        # remove the no_variant attributes that don't belong to this template
-        for ptav in self.product_no_variant_attribute_value_ids:
-            if ptav._origin not in valid_values:
-                self.product_no_variant_attribute_value_ids -= ptav
-
-        vals = {}
-        if not self.product_uom or (self.product_id.uom_id.id != self.product_uom.id):
-            vals['product_uom'] = self.product_id.uom_id
-            vals['product_uom_qty'] = self.product_uom_qty or 1.0
-
-        product = self.product_id.with_context(
-            lang=get_lang(self.env, self.order_id.partner_id.lang).code,
-            partner=self.order_id.partner_id,
-            quantity=vals.get('product_uom_qty') or self.product_uom_qty,
-            date=self.order_id.date_order,
-            pricelist=self.order_id.pricelist_id.id,
-            uom=self.product_uom.id
-        )
-
-        #vals.update(name=self.get_sale_order_line_multiline_description_sale(product))
-        #HEM NEW
-        vals.update(name=self.product_id.name)
-        #self.tax_id = self.order_id.partner_id.taxes_id # Added to get tax from Customer 
-        self._compute_tax_id() # commented to not to derive tax from product
-
-        if self.order_id.pricelist_id and self.order_id.partner_id:
-            vals['price_unit'] = self.env['account.tax']._fix_tax_included_price_company(self._get_display_price(product), product.taxes_id, self.tax_id, self.company_id)
-        self.update(vals)
-
-        title = False
-        message = False
-        result = {}
-        warning = {}
-        if product.sale_line_warn != 'no-message':
-            title = _("Warning for %s") % product.name
-            message = product.sale_line_warn_msg
-            warning['title'] = title
-            warning['message'] = message
-            result = {'warning': warning}
-            if product.sale_line_warn == 'block':
-                self.product_id = False
-
-        return result
-    
 class Opportunity2Quotation(models.TransientModel):
     _inherit = 'crm.quotation.partner'
     
