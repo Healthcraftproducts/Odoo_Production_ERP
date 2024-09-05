000b78a38661f910c2539174ac7d3e20f9949d98
sasindran-nits <sasindran.s@navabrindit.com>
2024-09-05 21:01:06 +0530

    production fix added

--- account_tax_python_ept/models/sale_order.py
+++ account_tax_python_ept/models/sale_order.py
@@ -8,8 +8,8 @@ class sale_order(models.Model):
         for order in self:
             if order.currency_id:
                 order_lines = order.order_line.filtered(lambda x: not x.display_type)
-            order.tax_totals = self.env['account.tax']._prepare_tax_totals(
-                [x.with_context({'tax_computation_context':{
-                    'line_tax_amount_percent':x.line_tax_amount_percent}})._convert_to_tax_base_line_dict() for x in order_lines],
-                order.currency_id,
-            )
+                order.tax_totals = self.env['account.tax']._prepare_tax_totals(
+                    [x.with_context({'tax_computation_context':{
+                        'line_tax_amount_percent':x.line_tax_amount_percent}})._convert_to_tax_base_line_dict() for x in order_lines],
+                    order.currency_id,
+                )
