68019c846db3a17b83ef348f9d4108a92f0e4ab2
sasindran-nits <sasindran.s@navabrindit.com>
2024-09-25 15:54:34 +0530

    purolator

--- purolator_shipping_integration/models/sale_order.py
+++ purolator_shipping_integration/models/sale_order.py
@@ -6,6 +6,12 @@ class SaleOrder(models.Model):
     purolator_shipping_charge_ids = fields.One2many("purolator.shipping.charge", "sale_order_id", string="Purolator Rate ")
     purolator_shipping_charge_id = fields.Many2one("purolator.shipping.charge", string="Purolator Service", copy=False)
 
+    purolator_third_party_account_number_sale_order = fields.Char(copy=False, string='Purolator Third-Party Account Number',
+                                                              help="Please Enter the Third Party account number ")
+    purolator_bill_by_third_party_sale_order = fields.Boolean(string="Purolator Third Party Payment",
+                                                              copy=False, default=False,
+                                                          help="when this fields is true,then we can visible purolator_third party account number")
+
     # def set_delivery_line(self, carrier, amount):
     #     # Remove delivery products from the sales order
     #     self._remove_delivery_line()
