82330aeb8c1ea0f8baa49525c4468904411cc7eb
sasindran-nits <sasindran.s@navabrindit.com>
2024-10-17 14:05:15 +0530

    temporary barcode check fix added

--- hcp_barcode_ext/models/stock_move_line.py
+++ hcp_barcode_ext/models/stock_move_line.py
@@ -5,8 +5,18 @@ from odoo.exceptions import ValidationError
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
+    # def button_validate(self):
+    #     for line in self.move_line_ids:
+    #         if line.qty_done > line.reserved_uom_qty:
+    #             raise ValidationError('The quantity done cannot exceed the product quantity.')
+    #     return super(StockPicking, self).button_validate()
+
+
     def button_validate(self):
-        for line in self.move_line_ids:
-            if line.qty_done > line.reserved_uom_qty:
-                raise ValidationError('The quantity done cannot exceed the product quantity.')
+        total_qty_done = sum(line.qty_done for line in self.move_line_ids)
+        total_reserved_uom_qty = sum(line.reserved_uom_qty for line in self.move_line_ids)
+
+        if total_qty_done > total_reserved_uom_qty:
+            raise ValidationError('The total quantity done cannot exceed the total reserved quantity.')
+
         return super(StockPicking, self).button_validate()
