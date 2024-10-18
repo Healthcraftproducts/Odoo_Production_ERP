29b52d1c3a93ecbcd67951efbecfd02cd9c186e4
sasindran-nits <sasindran.s@navabrindit.com>
2024-10-19 00:05:42 +0530

    sales ext added

---
+++ hcp_sales_ext/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "HCP Sales Extended",
+
+    'summary': """This module is used for sale order customization""",
+
+    'description': """This module is used for sale order customization""",
+
+    'author': "Navabrind IT Solutions Pvt Limited",
+    'website': "http://www.navabrindsol.com",
+    'category': 'Sales',
+    'version': '0.1',
+    'depends': ['base','sale'],
+    "application": False,
+    "installable": True,
+    'license': 'LGPL-3'
+}
