2b0daa347a05aac15e8d7862fb8a04f3c797db87
sasindran-nits <sasindran.s@navabrindit.com>
2024-10-22 18:32:21 +0530

    barcode and product changes added

--- hcp_barcode_ext/models/stock_move_line.py
+++ hcp_barcode_ext/models/stock_move_line.py
@@ -13,10 +13,26 @@ class StockPicking(models.Model):
 
 
     def button_validate(self):
-        total_qty_done = sum(line.qty_done for line in self.move_line_ids)
-        total_reserved_uom_qty = sum(line.reserved_uom_qty for line in self.move_line_ids)
+        if self.picking_type_id.code == 'internal':
+            total_qty_done = sum(line.qty_done for line in self.move_line_ids)
+            total_reserved_uom_qty = sum(line.reserved_uom_qty for line in self.move_line_ids)
+            if total_qty_done > total_reserved_uom_qty:
+                raise ValidationError('The total quantity done cannot exceed the total reserved quantity.')
 
-        if total_qty_done > total_reserved_uom_qty:
-            raise ValidationError('The total quantity done cannot exceed the total reserved quantity.')
-
+        if self.picking_type_id.code == 'outgoing':
+            for line in self.move_line_ids:
+                if self.origin:
+                    related_pickings = self.env['stock.picking'].search([
+                        ('origin', '=', self.origin),
+                        ('id', '!=', self.id),
+                        ('picking_type_id.code', '=', 'internal')
+                    ])
+                    if related_pickings:
+                        related_lots = related_pickings.mapped('move_line_ids.lot_id')
+                        if related_lots and line.lot_id:
+                            if line.lot_id not in related_lots:
+                                raise ValidationError(
+                                    'The lot number used in this order must match the lot number used in the related pickings.')
+                if line.qty_done > line.reserved_uom_qty:
+                    raise ValidationError('The quantity done cannot exceed the reserved quantity.')
         return super(StockPicking, self).button_validate()
