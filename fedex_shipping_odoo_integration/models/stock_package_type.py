e2ca62b1866f2f5f739f27c651c8cec2e952dc63
sasindran-nits <sasindran.s@navabrindit.com>
2024-10-24 20:54:37 +0530

    archive option added

--- fedex_shipping_odoo_integration/models/stock_package_type.py
+++ fedex_shipping_odoo_integration/models/stock_package_type.py
@@ -6,3 +6,4 @@ class StockPackageType(models.Model):
 
     package_carrier_type = fields.Selection(selection_add=[('fedex_shipping_provider', 'Fedex')])
     weight_uom_name = fields.Char()
+    active = fields.Boolean(string='Active', default=True)
